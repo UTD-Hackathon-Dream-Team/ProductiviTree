ddae9187490506517021000d3a06b9aec2c92daa
SaloniSS <sxs170015@utdallas.edu>
2020-06-27 19:08:14 -0500

    Setup expo-google-auth

--- app/components/GoogleAuth.tsx
+++ app/components/GoogleAuth.tsx
@@ -3,6 +3,9 @@
 import React, { useState, useEffect } from 'react';
 import { TouchableOpacity , Image } from 'react-native';
 import { Container, Content, Text, Button , Thumbnail } from "native-base";
+import * as Google from "expo-google-app-auth";
+
+import { ANDROID_CLIENT_ID, IOS_CLIENT_ID , ANDROID_APK_CLIENT_ID , IOS_APP_CLIENT_ID} from "../config";
 
 const GoogleAuth = ({navigate} : { navigate: any}) => {
 
