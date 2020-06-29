eda91af02b8dc6d2af3c2657e96efb3b4192e019
SaloniSS <sxs170015@utdallas.edu>
2020-06-28 21:34:31 -0500

    Change context on login and logout

--- app/components/GoogleLogOut.tsx
+++ app/components/GoogleLogOut.tsx
@@ -1,36 +1,39 @@
 //Source for google auth: https://docs.expo.io/versions/latest/sdk/google/
 
-import React, { useState, useEffect } from 'react';
+import React, { useContext } from 'react';
 import { TouchableOpacity , Image } from 'react-native';
-import { Container, Content, Text, Button , Thumbnail } from "native-base";
+import { Content } from "native-base";
 import * as Google from "expo-google-app-auth";
+import {context} from "../context" ;
 
 import { ANDROID_CLIENT_ID, IOS_CLIENT_ID , ANDROID_APK_CLIENT_ID , IOS_APP_CLIENT_ID} from "../config";
 
 const GoogleLogOut = ({navigate} : { navigate: any}) => {
 
     const signOutWithGoogle = async () => {
-        try {
-            const { type, accessToken, user } = await Google.logOutAsync({
-                accessToken: "",
-                iosClientId: IOS_CLIENT_ID,
-                androidClientId: ANDROID_CLIENT_ID,
-                androidStandaloneAppClientId: ANDROID_APK_CLIENT_ID,
-                iosStandaloneAppClientId: IOS_APP_CLIENT_ID,
-                scopes: ["profile", "email"],
-            });
-      
-            if (type === "success") {
-              console.log("Logged out", user);
-              navigate.navigate('Login');
-              return accessToken;
-            } else {
-              return { cancelled: true };
-            }
-          } catch (e) {
-            console.log("Error with logout", e);
-            return { error: true };
-          }
+      const auth = useContext(context);
+      auth.logout();
+      try {
+        const { type, accessToken, user } = await Google.logOutAsync({
+            accessToken: auth.accesstoken,
+            iosClientId: IOS_CLIENT_ID,
+            androidClientId: ANDROID_CLIENT_ID,
+            androidStandaloneAppClientId: ANDROID_APK_CLIENT_ID,
+            iosStandaloneAppClientId: IOS_APP_CLIENT_ID,
+            scopes: ["profile", "email"],
+        });
+  
+        if (type === "success") {
+          console.log("Logged out", user);
+          navigate.navigate('Login');
+          return accessToken;
+        } else {
+          return { cancelled: true };
+        }
+      } catch (e) {
+        console.log("Error with logout", e);
+        return { error: true };
+      }
     };
 
     return (
