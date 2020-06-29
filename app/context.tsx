bc8dbc80019171ff58237508f9670de394ebb132
SaloniSS <sxs170015@utdallas.edu>
2020-06-28 21:32:15 -0500

    Change js to tsx

--- app/context.tsx
+++ app/context.tsx
@@ -1,9 +1,9 @@
 import { createContext } from "react";
 
-export const Context = createContext({
+export const context = createContext({
   isLoggedIn: false,
   accesstoken: null,
   googleID: null,
-  login: () => {},
+  login: (googleID, accesstoken) => {},
   logout: () => {},
 });
