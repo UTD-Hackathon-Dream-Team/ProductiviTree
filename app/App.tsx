ea9476b5540e6e37ced5bd2774ede5bebf935e52
SaloniSS <sxs170015@utdallas.edu>
2020-06-28 21:34:17 -0500

    Set callback to implement context

--- app/App.tsx
+++ app/App.tsx
@@ -1,12 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect , useCallback } from 'react';
 import { AppLoading } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import TopStack from './navigation/TopStack';
+import {context} from "./context" ;
 
 const App = () => {
   const [ready, setReady] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userId, setUserId] = useState(null);
+  const [token, setToken] = useState(null);
+
+  const login = useCallback((googleID, accesstoken) => {
+    // useCallback so not rerendered
+    setIsLoggedIn(true);
+    setUserId(googleID);
+    setToken(accesstoken);
+  }, []);
+  
+  const logout = useCallback(() => {
+    setIsLoggedIn(false);
+    setUserId(null);
+    setToken(null);
+  }, []);
+  
 
   useEffect(() => {
     async function load() {
@@ -25,9 +43,19 @@ const App = () => {
   }
 
   return (
-    <NavigationContainer>
-      <TopStack/>
-    </NavigationContainer>
+    <context.Provider
+      value={{
+      isLoggedIn: !!token,
+      accesstoken: token,
+      googleID: userId,
+      login: login,
+      logout: logout,
+      }}
+      >
+      <NavigationContainer>
+        <TopStack/>
+      </NavigationContainer>
+    </context.Provider>
   );
 }
 
