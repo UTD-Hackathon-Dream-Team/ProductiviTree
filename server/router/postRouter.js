8f33f8ff96359e9f87ebb16bea1f8136d05896f3
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-07-01 16:00:25 -0500

    import functions

--- server/router/postRouter.js
+++ server/router/postRouter.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const router = express.Router();
-const postControllers = require("../controller/userController");
+const {
+  getPosts,
+  addPost,
+  getPost,
+  updatePost,
+  deletePost,
+  getUserPost,
+} = require("../controller/userController");
 
-router.route("/").get(postControllers.getPosts).post(postControllers.addPost);
+router.route("/").get(getPosts).post(addPost);
 
-router
-  .route("/:id")
-  .get(postControllers.getPost)
-  .patch(postControllers.updatePost)
-  .delete(postControllers.deletePost);
+router.route("/:id").get(getPost).patch(updatePost).delete(deletePost);
 
-router.route("/user/:userid").get(postControllers.getUserPost);
+router.route("/user/:userid").get(getUserPost);
 
 module.exports = router;
