18d89e61e5da502c3d27ba43310f3b3b5d9642e2
SaloniSS <sxs170015@utdallas.edu>
2020-07-07 01:18:55 -0500

    Friend Profile 1

---
+++ app/screens/FriendProfile.tsx
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Container, Content, Text, View, Button } from "native-base";
+import { ScrollView, RefreshControl, StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { AuthContext } from "../AuthContext";
+import ProfileInfo from "../components/ProfileInfo";
+import Post from "../components/PostList";
+
+const axios = require("axios").default;
+
+const FriendProfile = () => {
+  const auth = useContext(AuthContext);
+  let [user, setUser] = useState(null);
+  let [refreshing, setRefreshing] = useState(false);
+
+  async function fetchData(props) {
+    setRefreshing(true);
+    const result = await axios(
+      `https://productivitree.wl.r.appspot.com/api/v1/users/${props.user}`
+    );
+    setUser(result.data.payload);
+    setRefreshing(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const onRefresh = React.useCallback(async () => {
+    await fetchData();
+  }, [refreshing]);
+
+  function followUser(user) {
+    console.log(`Following user ${user.googleID} now`);
+  }
+
+  return (
+    <LinearGradient colors={["#C8F0EE", "#A1C6F1"]} style={{ flex: 1 }}>
+      <ScrollView
+        refreshControl={
+          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+        }
+      >
+        {user && <ProfileInfo user={user} />}        
+        <Button
+          style={{ justifyContent: "center", alignItems: "center" }}
+          //onPress={followUser(user)}
+        >
+          <Text>Un/follow</Text>
+        </Button>
+        {user && <Post user={user} />}
+      </ScrollView>
+    </LinearGradient>
+  );
+};
+
+export default FriendProfile;
