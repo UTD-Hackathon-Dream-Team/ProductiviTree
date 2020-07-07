d62de416457d91584ce5055dbd814c855efa3a90
SaloniSS <sxs170015@utdallas.edu>
2020-07-07 11:01:39 -0500

    Add ui for follow/unfollow button

--- app/screens/FriendProfile.tsx
+++ app/screens/FriendProfile.tsx
@@ -11,6 +11,7 @@ const axios = require("axios").default;
 const FriendProfile = (props) => {
   const auth = useContext(AuthContext);
   let [user, setUser] = useState(null);
+  let [following, setFollowing] = useState([]);
   let [refreshing, setRefreshing] = useState(false);
 
   async function fetchData() {
@@ -19,20 +20,33 @@ const FriendProfile = (props) => {
       `https://productivitree.wl.r.appspot.com/api/v1/users/112522383689004928445`
     );
     setUser(result.data.payload);
+    const response = await axios(
+      `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`
+    );
+    setFollowing(response.data.payload.Following);
     setRefreshing(false);
   }
 
   useEffect(() => {
     fetchData();
+    console.log(user.googleID);
+    console.log(following);
   }, []);
 
   const onRefresh = React.useCallback(async () => {
     await fetchData();
   }, [refreshing]);
 
-  function followUser() {
-    console.log(`Un/Following user ${user.googleID} now`);
-  }
+  const followUser = React.useCallback(async () => {
+    console.log(`Following user ${user.googleID} now`);
+    await fetchData();
+  }, [refreshing]);
+
+  const unfollowUser = React.useCallback(async () => {
+    console.log(`Unfollowing user ${user.googleID} now`);
+    await fetchData();
+  }, [refreshing]);
+
 
   return (
     <LinearGradient colors={["#C8F0EE", "#A1C6F1"]} style={{ flex: 1 }}>
@@ -41,13 +55,27 @@ const FriendProfile = (props) => {
           <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
       >
-        {user && <ProfileInfo user={user} />}        
-        <Button
-          style={{ justifyContent: "center", alignItems: "center" }}
-          onPress={followUser}
-        >
-          <Text>Un/follow</Text>
-        </Button>
+        {user && <ProfileInfo user={user} />}
+        {
+          user && following.includes(user.googleID) ? 
+          <View>
+            <Button
+              style={{ justifyContent: "center", alignItems: "center" }}
+              onPress={unfollowUser}
+            >
+              <Text>Unfollow</Text>
+            </Button>
+          </View>
+          :
+          <View>
+            <Button
+              style={{ justifyContent: "center", alignItems: "center" }}
+              onPress={followUser}
+            >
+              <Text>Follow</Text>
+            </Button>
+          </View>
+        }        
         {user && <Post user={user} />}
       </ScrollView>
     </LinearGradient>
