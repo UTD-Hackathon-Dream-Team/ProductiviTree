8650857db3e923961def82bdf465a2de41f499a9
SaloniSS <sxs170015@utdallas.edu>
2020-07-09 11:25:24 -0500

    add icon

--- server/model/activityModel.js
+++ server/model/activityModel.js
@@ -1,18 +1,22 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const ActivitySchema = new mongoose.Schema({
-    activity: {
-        type: String,
-        required: [true, 'Add an activity']
-    },
-    category: {
-        type: String,
-        required: [true, 'Add a category']
-    },
-    points: {
-        type: Number,
-        required: [true, 'Add the number of points']
-    }
+  activity: {
+    type: String,
+    required: [true, "Add an activity"],
+  },
+  category: {
+    type: String,
+    required: [true, "Add a category"],
+  },
+  points: {
+    type: Number,
+    required: [true, "Add the number of points"],
+  },
+  icon: {
+    type: String,
+    required: [true, "Add an icon"],
+  },
 });
 
-module.exports = mongoose.model('Activity', ActivitySchema);
+module.exports = mongoose.model("Activity", ActivitySchema);
