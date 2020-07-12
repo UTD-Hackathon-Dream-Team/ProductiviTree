c6e5335b0d3b45b8fb04f2453fb43223f0d197c0
Reshmi Ranjith <rreshmiranjith@gmail.com>
2020-07-12 11:34:32 -0500

    make feed not call state just set

--- app/screens/Feed.tsx
+++ app/screens/Feed.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Container, Content, Text , View} from "native-base";
+import { Container, Content, Text, View } from "native-base";
 import { ScrollView, RefreshControl, StyleSheet } from "react-native";
 import { LinearGradient } from "expo-linear-gradient";
 
@@ -12,60 +12,50 @@ const axios = require("axios").default;
 const Feed = () => {
   const auth = useContext(AuthContext);
   let [refreshing, setRefreshing] = useState(false);
-  let [following, setFollowing] = useState([]);
-  let [posts, setPosts] = useState([]);
-  let [feed, setFeed] = useState([]);
+  let [feed, setFeed] = useState(null);
 
-  async function fetchData() {
-    //setRefreshing(true);
+  const fetchData = React.useCallback(async () => {
+    setRefreshing(true);
     const result = await axios(
       `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`
     );
-    setFollowing(result.data.payload.Following);
+    const following = result.data.payload.Following;
     const response = await axios(
       `https://productivitree.wl.r.appspot.com/api/v1/posts`
     );
-    setPosts(response.data.payload);
-  }
-
-  async function adjustData() {
-    const newFeed = [];
-    await posts.forEach(post => {
+    const posts = response.data.payload;
+    const newFeed: [] = [];
+    posts.forEach((post: { Author: String }) => {
       let author = post.Author;
-      if (following.includes(author)){
+      if (following.includes(author)) {
         newFeed.push(post);
       }
     });
     setFeed(newFeed.reverse());
-    //setRefreshing(false);
-  }
+    setRefreshing(false);
+  }, []);
 
   useEffect(() => {
     async function loadData() {
-      // await fetchData();
-      // await adjustData();
-      await onRefresh();
+      await fetchData();
     }
     loadData();
   }, []);
 
-  const onRefresh = React.useCallback(async () => {
-    setRefreshing(true);
-    await fetchData();
-    await adjustData();
-    setRefreshing(false);
-    console.log(following);
-    console.log(feed);
-  }, [refreshing]);
-
   return (
-    <LinearGradient colors={["#C8F0EE", "#c8e2f1", "#A1C6F1"]} style={{ flex: 1 }}>
+    <LinearGradient
+      colors={["#C8F0EE", "#c8e2f1", "#A1C6F1"]}
+      style={{ flex: 1 }}
+    >
       <ScrollView
         refreshControl={
-          <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+          <RefreshControl
+            refreshing={refreshing}
+            onRefresh={async () => await fetchData()}
+          />
         }
       >
-        {feed && <FeedList posts={feed}/>}
+        {feed && <FeedList posts={feed} />}
       </ScrollView>
     </LinearGradient>
   );
