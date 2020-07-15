5963d720c4ddd5286a57e5c237526b073c1efaf7
SaloniSS <sxs170015@utdallas.edu>
2020-07-15 13:39:27 -0500

    get tree info

--- app/components/Stats.tsx
+++ app/components/Stats.tsx
@@ -9,14 +9,18 @@ const axios = require("axios").default;
 const Stats = () => {
   const auth = useContext(AuthContext);
   let [user, setUser] = useState(null);
+  let [trees, setTrees] = useState(0);
   let [refreshing, setRefreshing] = useState(false);
 
   async function fetchData() {
     setRefreshing(true);
-    const result = await axios(
-      `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`
-    );
+    const result = await axios( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`);
     setUser(result.data.payload);
+    const response = await axios(`https://productivitree.wl.r.appspot.com/api/v1/users`);
+    let allUsers = response.data.payload;
+    let totalTress = 0;
+    allUsers.forEach((otherUser: { Trees: Number }) => { totalTress += otherUser.Trees });
+    setTrees(totalTress);
     setRefreshing(false);
   }
 
@@ -35,23 +39,29 @@ const Stats = () => {
         }
         contentContainerStyle={{alignItems: "center"}}
     >
-        <View style={{alignItems: "center", paddingVertical: 20}}>
-            <Text>X / Y Daily Goal Points</Text>
-            <Text>Graph</Text>
-            <Text>Z% of Daily Goal Achieved</Text>
-        </View>
+        {user && 
+            <View style={{alignItems: "center", paddingVertical: 20}}>
+                <Text>X / Y Daily Goal Points</Text>
+                <Text>Graph</Text>
+                <Text>Z% of Daily Goal Achieved</Text>
+            </View>
+        }
         
-        <View style={{alignItems: "center", paddingVertical: 20}}>
-            <Image source={tree} style={{ height: 300, width: 300 }}/>
-            <Text>{user.Trees} Trees planted</Text>
-            <Text>x Trees planted globally</Text>
-        </View>
+        {user && 
+            <View style={{alignItems: "center", paddingVertical: 20}}>
+                <Image source={tree} style={{ height: 300, width: 300 }}/>
+                <Text>{user.Trees} Trees planted</Text>
+                <Text>{trees} Trees planted globally</Text>
+            </View>
+        }
         
-        <View style={{alignItems: "center", paddingVertical: 20}}>
-            <Text>{user.Points} / 1000 Points</Text>
-            <Text>Graph</Text>
-            <Text>Z Points until we plant a tree for your efforts</Text>
-        </View>
+        {user &&
+            <View style={{alignItems: "center", paddingVertical: 20}}>
+                <Text>{user.Points} / 1000 Points</Text>
+                <Text>Graph</Text>
+                <Text> {1000 - user.Points} Points until we plant a tree for your efforts</Text>
+            </View>
+        }
 
         <View style={{alignItems: "center", paddingVertical: 20}}>
             <Text>Learn More About Tree Planting</Text>
