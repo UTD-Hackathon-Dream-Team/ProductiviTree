5f757c22eaffaabca085242107d6ebb2c05681e5
SaloniSS <sxs170015@utdallas.edu>
2020-07-15 20:34:02 -0500

    Styling

--- app/components/Stats.tsx
+++ app/components/Stats.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Text, View, Button } from "native-base";
-import { ScrollView, RefreshControl, Image, Linking} from "react-native";
+import { View, Button } from "native-base";
+import { ScrollView, RefreshControl, Image, Linking, Text} from "react-native";
 import { AuthContext } from "../AuthContext";
 import * as Progress from 'react-native-progress';
 import tree from "../assets/tree_icon.png";
@@ -38,37 +38,52 @@ const Stats = () => {
         refreshControl={
         <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
         }
-        contentContainerStyle={{alignItems: "center"}}
+        contentContainerStyle={{alignItems: "center" }}
     >
         {user && 
             <View style={{alignItems: "center", paddingVertical: 20}}>
-                <Text> {750} / {user.DailyGoal} Daily Goal Points</Text>
-                <Progress.Bar progress={750 / user.DailyGoal} width={200}/>
-                <Text> {750 / user.DailyGoal * 100}% of Daily Goal Achieved</Text>
+                <Text style={{fontSize: 20 }}> 
+                  <Text style={{fontWeight: "bold" }}> {750} / {user.DailyGoal} </Text> 
+                  Daily Goal Points
+                </Text>
+                <Progress.Bar progress={750 / user.DailyGoal} width={250}/>
+                <Text style={{fontSize: 20 }}> 
+                  <Text style={{fontWeight: "bold" }}> {750 / user.DailyGoal * 100}% </Text> 
+                  of Daily Goal Achieved
+                </Text>
             </View>
         }
         
         {user && 
             <View style={{alignItems: "center", paddingVertical: 20}}>
                 <Image source={tree} style={{ height: 300, width: 300 }}/>
-                <Text>{user.Trees} trees planted by you</Text>
-                <Text>{trees} trees planted globally</Text>
+                <Text style={{fontSize: 30 }}>
+                  <Text style={{fontWeight: "bold" }}> {user.Trees} </Text> 
+                  trees planted by you
+                </Text>
+                <Text style={{fontSize: 20 }}>
+                  <Text style={{fontWeight: "bold" }}> {trees} </Text> 
+                  trees planted globally
+                </Text>
             </View>
         }
         
         {user &&
             <View style={{alignItems: "center", paddingVertical: 20}}>
-                <Text>{user.Points} / 1000 Points</Text>
+                <Text style={{fontSize: 20 ,fontWeight: "bold"}}> {user.Points} / 1000 Points</Text>
                 <Progress.Bar progress={user.Points / 1000} width={200}/>
-                <Text> {1000 - user.Points} Points until we plant a tree for your efforts</Text>
+                <Text style={{fontSize: 20, textAlign: "center" }}> 
+                <Text style={{fontWeight: "bold" }}> {1000 - user.Points} </Text> 
+                  Points until we plant a tree for your efforts
+                </Text>
             </View>
         }
 
-        <View style={{alignItems: "center", paddingVertical: 20}}>
+        {/* <View style={{alignItems: "center", paddingVertical: 20}}>
             <Button onPress={ ()=>{ Linking.openURL('https://google.com')}}>
                 <Text>Learn More About Tree Planting</Text>
             </Button>
-        </View>
+        </View> */}
     </ScrollView>
   );
 };
