04773348a95f33393a7eb7889f8900cd96ba58aa
SaloniSS <sxs170015@utdallas.edu>
2020-07-17 12:20:31 -0500

    Fix tabs attempt 2

--- app/screens/AddPost.tsx
+++ app/screens/AddPost.tsx
@@ -10,74 +10,73 @@ import { AuthContext } from "../AuthContext";
 const axios = require("axios").default;
 
 const AddPost = (props) => {
-    const auth = useContext(AuthContext);
-    const activity = "5f087e4ec318a70007c375f3";
-    let [enteredText, setEnteredText] = useState("");
-    let [image, setImage] = useState("https://wp-rocket.me/wp-content/uploads/1/placeholder-feature-image.png");
-    let [img64, setImg64] = useState(null);
-    let [imageURL, setImageURL] = useState(null);
+  const auth = useContext(AuthContext);
+  const activity = "5f087e4ec318a70007c375f3";
+  let [enteredText, setEnteredText] = useState("");
+  let [image, setImage] = useState("https://wp-rocket.me/wp-content/uploads/1/placeholder-feature-image.png");
+  let [img64, setImg64] = useState(null);
+  let [imageURL, setImageURL] = useState(null);
 
-    const getPickerPermission = async () => {
-        if (Constants.platform.ios) {
-        const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
-        if (status !== "granted") {
-            alert("Sorry, we need camera roll permissions to make this work!");
-        }
-        }
-    };
-
-    const handleSubmit = async () => {
-      await submitPost();
+  const getPickerPermission = async () => {
+    if (Constants.platform.ios) {
+      const { status } = await Permissions.askAsync(Permissions.CAMERA_ROLL);
+      if (status !== "granted") {
+        alert("Sorry, we need camera roll permissions to make this work!");
+      }
     }
+  };
 
-    const submitPost = async () => {
-        console.log("Submit post");
-        getImageURL();
-        console.log("Image", imageURL);
-        await axios
-            .get(`https://productivitree.wl.r.appspot.com/api/v1/activities/${activity}`)
-            .then((response) => {
-                axios.post("https://productivitree.wl.r.appspot.com/api/v1/posts", {
-                    Author: auth.googleID,
-                    Picture: imageURL,
-                    Caption: enteredText,
-                    Activity: response.data.payload
-                });
-            })
-    };
+  const handleSubmit = async () => {
+    await submitPost();
+  }
 
-    const getImageURL = async () => {
-        const data = new FormData();
-        data.append("file", "data:image/jpeg;base64," + img64);
-        data.append("upload_preset", "productivitree");
-        data.append("cloud_name", "utd-hdt");
-        await fetch("https://api.cloudinary.com/v1_1/utd-hdt/image/upload", {
-          method: "post",
-          body: data,
-        })
-        .then((res) => res.json())
-        .then((data) =>  {
-            setImageURL(data.secure_url);
-        });
-      };
+  const submitPost = async () => {
+    console.log("Submit post");
+    getImageURL();
+    console.log("Image", imageURL);
+    await axios.get(`https://productivitree.wl.r.appspot.com/api/v1/activities/${activity}`)
+    .then((response) => {
+      axios.post("https://productivitree.wl.r.appspot.com/api/v1/posts", {
+        Author: auth.googleID,
+        Picture: imageURL,
+        Caption: enteredText,
+        Activity: response.data.payload
+      });
+    })
+  };
 
-    const pickImage = async () => {
-        try {
-        await getPickerPermission();
-        let result = await ImagePicker.launchImageLibraryAsync({
-            allowsEditing: true,
-            base64: true,
-            quality: 1,
-            aspect: [4, 3]
-        });
-        if (!result.cancelled) {
-            setImage(result.uri);
-            setImg64(result.base64);
-        }
-        } catch (E) {
-        console.log(E);
-        }
-    };
+  const getImageURL = async () => {
+    const data = new FormData();
+    data.append("file", "data:image/jpeg;base64," + img64);
+    data.append("upload_preset", "productivitree");
+    data.append("cloud_name", "utd-hdt");
+    await fetch("https://api.cloudinary.com/v1_1/utd-hdt/image/upload", {
+      method: "post",
+      body: data,
+    })
+    .then((res) => res.json())
+    .then((data) =>  {
+      setImageURL(data.secure_url);
+    });
+  };
+
+  const pickImage = async () => {
+    try {
+      await getPickerPermission();
+      let result = await ImagePicker.launchImageLibraryAsync({
+        allowsEditing: true,
+        base64: true,
+        quality: 1,
+        aspect: [4, 3]
+      });
+      if (!result.cancelled) {
+        setImage(result.uri);
+        setImg64(result.base64);
+      }
+    } catch (E) {
+      console.log(E);
+    }
+  };
 
   return (
     <Root>
