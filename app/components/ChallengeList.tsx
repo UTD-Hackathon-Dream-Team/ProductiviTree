222445a1d7525e85254b5e6970ea2725464e44b2
SaloniSS <sxs170015@utdallas.edu>
2020-07-20 22:23:04 -0500

    Attempt to show toast

--- app/components/ChallengeList.tsx
+++ app/components/ChallengeList.tsx
@@ -1,26 +1,47 @@
 import React from "react";
 import { Text, FlatList } from "react-native";
-import { Content, ListItem, Left, Right } from "native-base";
+import { Content, ListItem, Left, Right, Toast } from "native-base";
+import { TouchableOpacity } from "react-native";
+import { AuthContext } from "../AuthContext";
+
+const axios = require("axios").default;
 
 const listCard = ({ item }) => {
+
+    function challengeHandler() {
+      if (item.progress == item.goal){
+        console.log("Points given")
+      }
+      else{
+        console.log("No points");
+        // Toast.show({
+        //   text: `You haven't completed all tasks`,
+        //   buttonText: "Okay",
+        //   position: "bottom",
+        // });
+      }
+    }
     
     return (
-      <ListItem>
-        <Left style={{ flexDirection: "column"}}>
-            <Text style={{ fontWeight: "bold" }}>
-            {item.description}
-          </Text>
+        <ListItem>
+          <Left style={{ flexDirection: "column"}}>
+            <TouchableOpacity onPress={challengeHandler}>
+                <Text style={{ fontWeight: "bold" }}>
+                {item.description}
+              </Text>
+              
+              <Text style={{ fontWeight: "bold" }}>
+                {item.points} pts
+              </Text>
+              </TouchableOpacity>
+          </Left>
           
-          <Text style={{ fontWeight: "bold" }}>
-            {item.points} pts
-          </Text>
-        </Left>
-        <Right>
-            <Text style={{ fontWeight: "bold" }}>
-            {item.progress}/{item.goal}
-          </Text>
-        </Right>
-      </ListItem>
+          <Right>
+              <Text style={{ fontWeight: "bold" }}>
+              {item.progress}/{item.goal}
+            </Text>
+          </Right>
+        </ListItem>
     );
 }
 
