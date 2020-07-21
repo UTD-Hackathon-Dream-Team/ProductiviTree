2f6eb3786299b276dd0c5db3ba540335fe4fd222
SaloniSS <sxs170015@utdallas.edu>
2020-07-21 15:57:53 -0500

    Toast works now

--- app/components/ChallengeList.tsx
+++ app/components/ChallengeList.tsx
@@ -1,6 +1,6 @@
 import React, {useContext} from "react";
 import { Text, FlatList } from "react-native";
-import { Content, ListItem, Left, Right, Toast } from "native-base";
+import { Content, ListItem, Left, Right, Toast, Root } from "native-base";
 import { TouchableOpacity } from "react-native";
 import { AuthContext } from "../AuthContext";
 
@@ -11,64 +11,60 @@ const listCard = ({ item }) => {
   async function challengeHandler() {
     if (item.progress == item.goal){
       console.log("Points given");
-      // Toast.show({
-      //   text: `You have earned {item.points} points!`,
-      //   buttonText: "Okay",
-      //   position: "bottom",
-      // });
       
-      //TODO: Afters
-      // const auth = useContext(AuthContext);
-      // const newPoints = item.points;
-      // const userResponse = await axios(`https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`);
-      // const oldPoints = userResponse.data.payload.Points;
-      // const oldTrees = userResponse.data.payload.Trees;
-      // axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
-      //   {
-      //     Points: oldPoints + newPoints,
-      //   }
-      // )
-      // // Toast.show({
-      // //   text: `You've earned ${newPoints} points!`,
-      // //   buttonText: "Okay",
-      // //   position: "bottom",
-      // // });
-      // if ((oldPoints + newPoints) > 1000){
-      //   axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
-      //     {
-      //       Points: oldPoints + newPoints - 1000,
-      //       Trees: oldTrees + 1
-      //     }
-      //   )
-      //   // Toast.show({
-      //   //   text: `You've planted one more tree and earned ${newPoints} points!`,
-      //   //   buttonText: "Okay",
-      //   //   position: "bottom",
-      //   // });
-      // }
+      const auth = useContext(AuthContext);
+      const newPoints = item.points;
+      const userResponse = await axios(`https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`);
+      const oldPoints = userResponse.data.payload.Points;
+      const oldTrees = userResponse.data.payload.Trees;
+      axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
+        {
+          Points: oldPoints + newPoints,
+        }
+      )
+      Toast.show({
+        text: `You've earned ${newPoints} points!`,
+        buttonText: "Okay",
+        position: "bottom",
+      });
+      if ((oldPoints + newPoints) > 1000){
+        axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
+          {
+            Points: oldPoints + newPoints - 1000,
+            Trees: oldTrees + 1
+          }
+        )
+        Toast.show({
+          text: `You've planted one more tree and earned ${newPoints} points!`,
+          buttonText: "Okay",
+          position: "bottom",
+        });
+      }
     }
     else{
       console.log("No points");
-      // Toast.show({
-      //   text: `You haven't completed all tasks`,
-      //   buttonText: "Okay",
-      //   position: "bottom",
-      // });
+      Toast.show({
+        text: `You haven't completed all tasks`,
+        buttonText: "Okay",
+        position: "bottom",
+      });
     }
   }
     
   return (
       <ListItem>
         <Left style={{ flexDirection: "column"}}>
-          <TouchableOpacity onPress={challengeHandler}>
+          <Root>
+            <TouchableOpacity onPress={challengeHandler}>
+                <Text style={{ fontWeight: "bold" }}>
+                {item.description}
+              </Text>
+              
               <Text style={{ fontWeight: "bold" }}>
-              {item.description}
-            </Text>
-            
-            <Text style={{ fontWeight: "bold" }}>
-              {item.points} pts
-            </Text>
-            </TouchableOpacity>
+                {item.points} pts
+              </Text>
+              </TouchableOpacity>
+            </Root>
         </Left>
         
         <Right>
