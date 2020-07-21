d813b254a2414257402b61852103d681a84249d3
SaloniSS <sxs170015@utdallas.edu>
2020-07-20 22:28:54 -0500

    Some more ui stuff

--- app/components/ChallengeList.tsx
+++ app/components/ChallengeList.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Text, FlatList } from "react-native";
-import { Content, ListItem, Left, Right, Toast } from "native-base";
+import { Content, ListItem, Left, Right, Toast, View , Card, CardItem} from "native-base";
 import { TouchableOpacity } from "react-native";
 import { AuthContext } from "../AuthContext";
 
@@ -8,40 +8,82 @@ const axios = require("axios").default;
 
 const listCard = ({ item }) => {
 
-    function challengeHandler() {
-      if (item.progress == item.goal){
-        console.log("Points given")
+
+  const updatePoints = async () => {
+    const newPoints = item.points;
+    const userResponse = await axios(`https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`);
+    const oldPoints = userResponse.data.payload.Points;
+    const oldTrees = userResponse.data.payload.Trees;
+    axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
+      {
+        Points: oldPoints + newPoints,
       }
-      else{
-        console.log("No points");
-        // Toast.show({
-        //   text: `You haven't completed all tasks`,
-        //   buttonText: "Okay",
-        //   position: "bottom",
-        // });
+    )
+    .then(function (response) {
+      Toast.show({
+        text: `You've earned ${newPoints} points!`,
+        buttonText: "Okay",
+        position: "bottom",
+      });
+
+      if ((oldPoints + newPoints) > 1000){
+        axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
+          {
+            Points: oldPoints + newPoints - 1000,
+            Trees: oldTrees + 1
+          }
+        )
+        Toast.show({
+          text: `You've planted one more tree and earned ${newPoints} points!`,
+          buttonText: "Okay",
+          position: "bottom",
+        });
       }
+      setTimeout(() => {
+        console.log("Go to feed / previous page here");
+      }, 5000);
+    })
+  }
+
+  function challengeHandler() {
+    if (item.progress == item.goal){
+      console.log("Points given");
+      updatePoints();
     }
+    else{
+      console.log("No points");
+      // Toast.show({
+      //   text: `You haven't completed all tasks`,
+      //   buttonText: "Okay",
+      //   position: "bottom",
+      // });
+    }
+  }
     
     return (
-        <ListItem>
-          <Left style={{ flexDirection: "column"}}>
-            <TouchableOpacity onPress={challengeHandler}>
+      <Card>
+        {
+          item &&
+          <TouchableOpacity onPress={challengeHandler}>
+            <CardItem>
+              <Left style={{ flexDirection: "column"}}>
                 <Text style={{ fontWeight: "bold" }}>
-                {item.description}
-              </Text>
+                  {item.description}
+                </Text>      
+                <Text style={{ fontWeight: "bold" }}>
+                  {item.points} pts
+                </Text>
+              </Left>
               
-              <Text style={{ fontWeight: "bold" }}>
-                {item.points} pts
-              </Text>
-              </TouchableOpacity>
-          </Left>
-          
-          <Right>
-              <Text style={{ fontWeight: "bold" }}>
-              {item.progress}/{item.goal}
-            </Text>
-          </Right>
-        </ListItem>
+              <Right>
+                  <Text style={{ fontWeight: "bold" }}>
+                  {item.progress}/{item.goal}
+                </Text>
+              </Right>
+            </CardItem>
+          </TouchableOpacity>
+        }
+        </Card>
     );
 }
 
