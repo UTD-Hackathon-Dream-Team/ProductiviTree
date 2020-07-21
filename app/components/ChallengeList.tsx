599070ed31808dbe6b61cef0084a56ee2d574488
SaloniSS <sxs170015@utdallas.edu>
2020-07-20 22:49:09 -0500

    Tried to make backend work

--- app/components/ChallengeList.tsx
+++ app/components/ChallengeList.tsx
@@ -8,76 +8,76 @@ const axios = require("axios").default;
 
 const listCard = ({ item }) => {
 
-  async function updatePoints() {
-    //await console.log("updating points");
-    const auth = useContext(AuthContext);
-    const newPoints = item.points;
-    const userResponse = await axios(`https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`);
-    const oldPoints = userResponse.data.payload.Points;
-    const oldTrees = userResponse.data.payload.Trees;
-    axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
-      {
-        Points: oldPoints + newPoints,
-      }
-    )
-    .then(function (response) {
+  async function challengeHandler() {
+    if (item.progress == item.goal){
+      console.log("Points given");
       // Toast.show({
-      //   text: `You've earned ${newPoints} points!`,
+      //   text: `You have earned {item.points} points!`,
       //   buttonText: "Okay",
       //   position: "bottom",
       // });
-      if ((oldPoints + newPoints) > 1000){
-        axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
-          {
-            Points: oldPoints + newPoints - 1000,
-            Trees: oldTrees + 1
-          }
-        )
-        // Toast.show({
-        //   text: `You've planted one more tree and earned ${newPoints} points!`,
-        //   buttonText: "Okay",
-        //   position: "bottom",
-        // });
-      }
-    })
-  }
-
-    function challengeHandler() {
-      if (item.progress == item.goal){
-        console.log("Points given");
-        updatePoints();
-      }
-      else{
-        console.log("No points");
-        // Toast.show({
-        //   text: `You haven't completed all tasks`,
-        //   buttonText: "Okay",
-        //   position: "bottom",
-        // });
-      }
+      
+      //TODO: Afters
+      // const auth = useContext(AuthContext);
+      // const newPoints = item.points;
+      // const userResponse = await axios(`https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`);
+      // const oldPoints = userResponse.data.payload.Points;
+      // const oldTrees = userResponse.data.payload.Trees;
+      // axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
+      //   {
+      //     Points: oldPoints + newPoints,
+      //   }
+      // )
+      // // Toast.show({
+      // //   text: `You've earned ${newPoints} points!`,
+      // //   buttonText: "Okay",
+      // //   position: "bottom",
+      // // });
+      // if ((oldPoints + newPoints) > 1000){
+      //   axios.patch( `https://productivitree.wl.r.appspot.com/api/v1/users/${auth.googleID}`, 
+      //     {
+      //       Points: oldPoints + newPoints - 1000,
+      //       Trees: oldTrees + 1
+      //     }
+      //   )
+      //   // Toast.show({
+      //   //   text: `You've planted one more tree and earned ${newPoints} points!`,
+      //   //   buttonText: "Okay",
+      //   //   position: "bottom",
+      //   // });
+      // }
     }
+    else{
+      console.log("No points");
+      // Toast.show({
+      //   text: `You haven't completed all tasks`,
+      //   buttonText: "Okay",
+      //   position: "bottom",
+      // });
+    }
+  }
     
-    return (
-        <ListItem>
-          <Left style={{ flexDirection: "column"}}>
-            <TouchableOpacity onPress={challengeHandler}>
-                <Text style={{ fontWeight: "bold" }}>
-                {item.description}
-              </Text>
-              
+  return (
+      <ListItem>
+        <Left style={{ flexDirection: "column"}}>
+          <TouchableOpacity onPress={challengeHandler}>
               <Text style={{ fontWeight: "bold" }}>
-                {item.points} pts
-              </Text>
-              </TouchableOpacity>
-          </Left>
-          
-          <Right>
-              <Text style={{ fontWeight: "bold" }}>
-              {item.progress}/{item.goal}
+              {item.description}
             </Text>
-          </Right>
-        </ListItem>
-    );
+            
+            <Text style={{ fontWeight: "bold" }}>
+              {item.points} pts
+            </Text>
+            </TouchableOpacity>
+        </Left>
+        
+        <Right>
+            <Text style={{ fontWeight: "bold" }}>
+            {item.progress}/{item.goal}
+          </Text>
+        </Right>
+      </ListItem>
+  );
 }
 
 const Challenge = (props) => {
