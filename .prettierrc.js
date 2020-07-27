bdec4b7f51b7a233b0de4ccb3286e77fd6ef4b6f
SaloniSS <sxs170015@utdallas.edu>
2020-07-27 00:04:14 -0500

    prettier

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  trailingComma: "es5",
+  semi: true,
+  singleQuote: false,
+  jsxSingleQuote: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  arrowParens: "always",
+  printWidth: 100,
+  useTabs: false,
+};
