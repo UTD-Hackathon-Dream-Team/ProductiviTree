9a5cf2992d3e5467cbb7bdcc4281e8d2b06127ea
SaloniSS <sxs170015@utdallas.edu>
2020-08-06 03:51:21 -0500

    Buttons to change view

--- website/src/screens/Feed.js
+++ website/src/screens/Feed.js
@@ -1,5 +1,6 @@
-import React, { useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { AuthContext } from "../AuthContext";
+import { Button } from "@material-ui/core";
 import GoogleLogIn from "../components/GoogleLogIn";
 import GoogleLogOut from "../components/GoogleLogOut";
 import PublicFeed from "../components/PublicFeed";
@@ -8,15 +9,25 @@ import YourFeed from "../components/YourFeed";
 
 function Feed() {
   const auth = useContext(AuthContext);
+  const [view, setView] = useState(0);
 
   return (
     <div style={{ background: "linear-gradient(to right bottom, #C8F0EE, #A1C6F1)" }}>
       {auth.isLoggedIn ? (
         <div style={{ textAlign: "center" }}>
           <GoogleLogOut />
-          {/* <PublicFeed /> */}
-          <YourFeed />
-          {/* <YourPosts /> */}
+          <div>
+            <Button variant="outlined" color="primary" onClick={() => setView(0)}>
+              Public Feed
+            </Button>
+            <Button variant="outlined" color="primary" onClick={() => setView(1)}>
+              Your Feed
+            </Button>
+            <Button variant="outlined" color="primary" onClick={() => setView(2)}>
+              Your Posts
+            </Button>
+          </div>
+          <div>{view == 0 ? <PublicFeed /> : view == 1 ? <YourFeed /> : <YourPosts />}</div>
         </div>
       ) : (
         <div style={{ textAlign: "center" }}>
