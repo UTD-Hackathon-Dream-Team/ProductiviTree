af640c3cc1a4c2a7aaedf0597ba43da2bec487b7
SaloniSS <sxs170015@utdallas.edu>
2020-08-06 03:30:50 -0500

    View your posts

--- website/src/screens/Feed.js
+++ website/src/screens/Feed.js
@@ -3,6 +3,7 @@ import { AuthContext } from "../AuthContext";
 import GoogleLogIn from "../components/GoogleLogIn";
 import GoogleLogOut from "../components/GoogleLogOut";
 import PublicFeed from "../components/PublicFeed";
+import YourPosts from "../components/YourPosts";
 
 function Feed() {
   const auth = useContext(AuthContext);
@@ -10,12 +11,14 @@ function Feed() {
   return (
     <div style={{ background: "linear-gradient(to right bottom, #C8F0EE, #A1C6F1)" }}>
       {auth.isLoggedIn ? (
-        <div>
+        <div style={{ textAlign: "center" }}>
           <GoogleLogOut />
+          {/* <PublicFeed /> */}
+          <YourPosts />
         </div>
       ) : (
         <div style={{ textAlign: "center" }}>
-          <GoogleLogIn />
+          <GoogleLogIn /> To View Your Personalized Feed
           <PublicFeed />
         </div>
       )}
