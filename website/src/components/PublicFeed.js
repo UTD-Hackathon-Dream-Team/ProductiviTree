c47b25b30db7f3250e8828b85060b9348c5a0f50
SaloniSS <sxs170015@utdallas.edu>
2020-08-06 03:15:54 -0500

    change to grid

--- website/src/components/PublicFeed.js
+++ website/src/components/PublicFeed.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useContext } from "react";
 import Post from "./Post";
+import { Grid } from "@material-ui/core";
 const axios = require("axios").default;
 
 function PublicFeed() {
@@ -14,13 +15,13 @@ function PublicFeed() {
   }, []);
 
   return (
-    <div>
+    <Grid container spacing={2}>
       {posts.map((post) => (
         <div style={{ margin: 10 }}>
           <Post post={post._id} key={post._id} />
         </div>
       ))}
-    </div>
+    </Grid>
   );
 }
 
