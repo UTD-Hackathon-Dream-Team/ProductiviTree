c1dbc4ea159436f042c4d90be19838404ffe462e
SaloniSS <sxs170015@utdallas.edu>
2020-08-06 03:01:45 -0500

    Post card is done

--- website/src/components/Post.js
+++ website/src/components/Post.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Card, CardHeader, CardMedia } from "@material-ui/core";
+import { Card, CardHeader, CardMedia, CardContent } from "@material-ui/core";
 
+var moment = require("moment");
 const axios = require("axios").default;
 
 function Post(props) {
   const [post, setPost] = useState({});
+  const [activity, setActivity] = useState({});
   const [user, setUser] = useState({});
 
   useEffect(() => {
@@ -13,6 +15,7 @@ function Post(props) {
         `https://productivitree.wl.r.appspot.com/api/v1/posts/${props.post}`
       );
       setPost(result.data.payload);
+      setActivity(result.data.payload.Activity);
       const response = await axios(
         `https://productivitree.wl.r.appspot.com/api/v1/users/${result.data.payload.Author}`
       );
@@ -23,13 +26,24 @@ function Post(props) {
 
   return (
     <div>
-      <Card>
-        <CardHeader
-          avatar={<img style={{ height: 30, width: 30, borderRadius: 30 }} src={user.ProfilePic} />}
-          title={user.Username}
-        />
-        <CardMedia image={post.Picture} style={{ height: 300, width: 400, flex: 1 }} />
-      </Card>
+      {post && (
+        <Card>
+          <CardHeader
+            avatar={
+              <img style={{ height: 30, width: 30, borderRadius: 30 }} src={user.ProfilePic} />
+            }
+            title={user.Username}
+            subheader={moment(post.TimeStamp).fromNow()}
+          />
+          <CardMedia image={post.Picture} style={{ height: 300, width: 400, flex: 1 }} />
+          <CardContent>
+            <p>{post.Caption}</p>
+            <p>
+              Activity: {activity.Activity} ( {activity.Category} )
+            </p>
+          </CardContent>
+        </Card>
+      )}
     </div>
   );
 }
