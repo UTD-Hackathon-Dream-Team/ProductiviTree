91bfd7950db629ac5f853e2f6f3ee14dc4b836f2
SaloniSS <sxs170015@utdallas.edu>
2020-08-06 22:55:39 -0500

    Leaderboard Cards

--- website/src/components/BoardPosition.js
+++ website/src/components/BoardPosition.js
@@ -1,14 +1,28 @@
 import React from "react";
+import { Card, CardHeader, CardMedia, CardContent } from "@material-ui/core";
 
 function BoardPosition(props) {
   return (
-    <div>
-      <h2>{props.pos}</h2>
-      <h3>{props.user.Username}</h3>{" "}
-      <p>
-        Trees: {props.user.Trees} Points: {props.user.Points}
-      </p>
-    </div>
+    <Card style={{ width: "fit-content" }}>
+      <CardContent style={{ display: "flex" }}>
+        <h3> #{props.pos}</h3>
+        <CardMedia
+          image={props.user.ProfilePic}
+          style={{ height: 30, width: 30, borderRadius: 30 }}
+        />
+        <h4>{props.user.Username}</h4>
+        <CardMedia
+          image={require("../assets/tree_icon.png")}
+          style={{ height: 30, width: 30, borderRadius: 30 }}
+        />
+        <p>{props.user.Trees}</p>
+        <CardMedia
+          image={require("../assets/study_icon.png")}
+          style={{ height: 30, width: 30, borderRadius: 30 }}
+        />
+        <p>{props.user.Points}</p>
+      </CardContent>
+    </Card>
   );
 }
 
