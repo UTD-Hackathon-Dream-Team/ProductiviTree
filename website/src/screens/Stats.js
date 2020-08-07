5ae145c93b2a1de0e7f93abfadd1c2b3cb1b450c
SaloniSS <sxs170015@utdallas.edu>
2020-08-06 22:59:02 -0500

    Buttons to shift views

--- website/src/screens/Stats.js
+++ website/src/screens/Stats.js
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
+import { Button } from "@material-ui/core";
 import { AuthContext } from "../AuthContext";
 import GoogleLogIn from "../components/GoogleLogIn";
 import GoogleLogOut from "../components/GoogleLogOut";
@@ -6,12 +7,22 @@ import PublicLeaderBoard from "../components/PublicLeaderBoard";
 
 function Stats() {
   const auth = useContext(AuthContext);
+  const [view, setView] = useState(0);
 
   return (
     <div style={{ background: "linear-gradient(to right bottom, #C8F0EE, #A1C6F1)" }}>
       {auth.isLoggedIn ? (
         <div>
           <GoogleLogOut />
+          <div>
+            <Button variant="outlined" color="primary" onClick={() => setView(0)}>
+              Public Stats
+            </Button>
+            <Button variant="outlined" color="primary" onClick={() => setView(1)}>
+              Your Stats
+            </Button>
+          </div>
+          <div>{view == 0 ? <PublicLeaderBoard /> : <PublicLeaderBoard />}</div>
         </div>
       ) : (
         <div>
