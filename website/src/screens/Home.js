385054a8ebd2d2004a833dc690e001ae47160118
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2020-08-08 19:25:57 -0500

    fixed something

--- website/src/screens/Home.js
+++ website/src/screens/Home.js
@@ -2,8 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div style={{  height: "100vh",
-      width: "100vw"}}>
+    <div style={{  height: "100vh"}}>
       <h1> Home </h1>
     </div>
   );
