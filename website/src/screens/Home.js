2a739168d19e425aa6e473823c1d270c319f8655
SaloniSS <sxs170015@utdallas.edu>
2020-08-09 08:15:41 -0500

    video placeholder

--- website/src/screens/Home.js
+++ website/src/screens/Home.js
@@ -1,9 +1,10 @@
 import React from "react";
+import Iframe from "react-iframe";
 
 function Home() {
   return (
-    <div style={{  height: "100vh"}} /* fill screen for now */>
-      <h1> Home </h1>
+    <div style={{ height: "100vh" }} /* fill screen for now */>
+      <Iframe url="http://www.youtube.com/embed/r2eyxjckIYY" width="100%" height="100%" />
     </div>
   );
 }
