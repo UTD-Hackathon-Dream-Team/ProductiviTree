95bf27c3336eb64228c37f82fb68b7c81f4a02b1
CuteMustard <55762397+CuteMustard@users.noreply.github.com>
2020-08-08 19:28:27 -0500

    Made the body of some pages fill screen since theres nothing there right now

--- website/src/screens/Home.js
+++ website/src/screens/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 function Home() {
   return (
-    <div style={{  height: "100vh"}}>
+    <div style={{  height: "100vh"}} /* fill screen for now */>
       <h1> Home </h1>
     </div>
   );
